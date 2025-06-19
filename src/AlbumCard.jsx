40fb9891271fef05cc0cd1ce52321ae84f240234
brunston97 <brunston97@gmail.com>
2025-06-19 17:00:54 -0500

    added images, set colors

--- src/AlbumCard.jsx
+++ src/AlbumCard.jsx
@@ -3,14 +3,15 @@ import { Card, CardBody, CardHeader, Image } from '@nextui-org/react'
 const AlbumCard = ({ title, data, counts }) => {
     console.log('AlbumCard props:', { title, data });
     return (
-        <Card isPressable className="flex flex-col">
+        <Card className="flex flex-col bg-zinc-500 p-5 " style={{ borderRadius: '1rem' }}>
             <CardHeader className="mb-4">
-                <h2 className="text-xl font-semibold">{title}</h2>
+                <h2 className="text-xl text-white font-semibold">{title}</h2>
             </CardHeader>
             <CardBody>
+                <img src={data.cover} className="mb-4"></img>
                 <ul>
                     {data.songs?.map((song) => (
-                        <li key={song} className="flex justify-between items-center py-1">
+                        <li key={song} className="flex justify-between items-center py-1 text-white">
                             <span>{song}</span>
                             <span>{counts[song] ?? 0}</span>
                         </li>
